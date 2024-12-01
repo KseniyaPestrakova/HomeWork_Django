1dbcf7b489f6e40a94240e26f6eb048f20f34264
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-12-01 21:35:38 +0300

    add new app catalog

---
+++ catalog/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def home(request):
+    if request.method == 'GET':
+        return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'GET':
+        return render(request, 'catalog/contacts.html')
