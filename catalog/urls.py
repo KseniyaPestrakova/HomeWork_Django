7b36a3b516e7dc7b97180aa213c975bdadeeeb45
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2024-12-23 01:52:10 +0300

    add new possibilities

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-
+from .views import home, product_detail
 
 app_name = "catalog"
 
-urlpatterns = [path("home/", views.home, name="home"), path("contacts/", views.contacts, name="contacts")]
+urlpatterns = [path('', home, name="home"),
+               path('products/<int:pk>/', product_detail, name="product_detail"),
+               path("contacts/", views.contacts, name="contacts"),
+               ]
--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 from config import settings
 
@@ -25,5 +26,5 @@ urlpatterns = [
     path("", include("catalog.urls", namespace="catalog")),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
