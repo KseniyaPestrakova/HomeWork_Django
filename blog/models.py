a1dd5c595e93a300efffdd512261ec94e2da49b4
Kseniya Pestrakova <pestrakowa.k@yandex.ru>
2025-01-05 21:06:02 +0300

    add new app 'blog'

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=300, verbose_name="Заголовок публикации")
+    content = models.TextField(blank=True, null=True)
+    preview = models.ImageField(upload_to="media/", blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+    views = models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "публикация"
+        verbose_name_plural = "публикации"
+        ordering = ["created_at"]
+
+
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,31 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-
 from catalog.models import Product
-
-from django.views.generic import ListView, DetailView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, TemplateView
 
 
-class MyProductsHomeListView(ListView):
+class ProductsHomeListView(ListView):
     model = Product
-    template_name = 'catalog/home_list.html'
-    context_object_name = 'mymodels'
 
 
-def home(request):
-    if request.method == "GET":
-        products = Product.objects.all()
-        context = {"products": products}
-        return render(request, "catalog/home.html", context)
+class ProductsDetailView(DetailView):
+    model = Product
 
 
-def contacts(request):
-    if request.method == "GET":
-        return render(request, "catalog/contacts.html")
-
-
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, "catalog/product_detail.html", context)
-
+class ProductsTemplateView(TemplateView):
+    model = Product
+    template_name = 'catalog/contacts.html'
+    context_object_name = 'product'
